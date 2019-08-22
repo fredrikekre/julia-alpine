e3da78abf80c4ccf4b21904d726d205ccc311f9b
Fredrik Ekre <ekrefredrik@gmail.com>
2019-08-23 00:09:12 +0200

    Update for building of Julia 1.2.0.

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-if [ "$1" = "v1.2.0-rc1" ]; then
-    patch -p1 -i "patches/fix-rpath.diff"
-    patch -p1 -i "patches/fix-getopt.diff"
-    patch -p1 -i "patches/revert-disable-BB-for-gmp-and-mpfr.diff"
+if [ "$1" = "v1.2.0" ]; then
     patch -p1 -i "patches/binarybuilder-for-dSFMT.diff"
-    patch -p1 -i "patches/binarybuilder-for-Objconv.diff";
-    patch -p1 -i "patches/disable-libunwind-test.diff";
+    patch -p1 -i "patches/binarybuilder-for-Objconv.diff"
+    patch -p1 -i "patches/disable-libunwind-test.diff"
+    echo 'override USE_BINARYBUILDER_LIBUNWIND=0' >> Make.user
+    echo 'override USE_BINARYBUILDER_UNWIND=0' >> Make.user;
 fi
-echo 'override USE_BINARYBUILDER_LIBUNWIND=0' >> Make.user
-echo 'override USE_BINARYBUILDER_UNWIND=0' >> Make.user
+echo 'override USE_BINARYBUILDER_GMP=1' >> Make.user
+echo 'override USE_BINARYBUILDER_MPFR=1' >> Make.user
 echo 'override TAGGED_RELEASE_BANNER="github.com/fredrikekre/julia-alpine build"' >> Make.user
